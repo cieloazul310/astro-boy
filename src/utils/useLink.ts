90be889caf8488ad19ae945c809b79f381a4adbf
cieloazul310 <cieloazul310@gmail.com>
2023-09-09 14:20:26 +0900

    feat: add Link component

---
+++ src/utils/useLink.ts
@@ -0,0 +1,18 @@
+function useLink(href: string | URL | undefined | null): {
+  href: string | URL | null;
+  target?: "_blank";
+  rel?: "noreferrer noopener";
+} {
+  const baseUrl = import.meta.env.BASE_URL;
+
+  if (!href) return { href: null };
+  const internal = /^\/(?!\/)/.test(href.toString());
+
+  if (!internal) return { href, target: "_blank", rel: "noreferrer noopener" };
+
+  if (baseUrl === "/") return { href };
+
+  return { href: `${baseUrl}${href}` };
+}
+
+export default useLink;
