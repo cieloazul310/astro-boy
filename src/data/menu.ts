7c311776be2f2192f3ebaced9676b83d65dcdc98
cieloazul310 <cieloazul310@gmail.com>
2023-09-11 15:45:58 +0900

    chore: update

---
+++ src/data/menu.ts
@@ -0,0 +1,9 @@
+const menu = [
+  { name: "Top", path: "/" },
+  { name: "About", path: "/about/" },
+  { name: "Components", path: "/components/" },
+  { name: "Markdown Posts", path: "/posts/" },
+  { name: "CMS Posts", path: "/microcms/" },
+];
+
+export default menu;
