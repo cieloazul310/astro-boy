3cd6741f70ec88345de79415d4b556a1e74165cb
cieloazul310 <cieloazul310@gmail.com>
2023-10-06 17:09:57 +0900

    feat: add mdx

--- src/utils/rehype.ts
+++ src/utils/rehype.ts
@@ -1,12 +1,21 @@
 /* eslint import/prefer-default-export: off */
 import { rehype } from "rehype";
 import addClasses from "rehype-class-names";
+import rehypeExternalLinks, {
+  type Options as RehypeExternalLinksOptions,
+} from "rehype-external-links";
 import { rehypeClassNamesOptions } from "./componentClasses";
+
+const rehypeExternalLinksOptions: RehypeExternalLinksOptions = {
+  target: "_blank",
+  rel: "noopener noreferrer",
+};
 
 export async function rehypeIt(input: string) {
   const output = await rehype()
     // @ts-expect-error
     .use(addClasses, rehypeClassNamesOptions)
+    .use(rehypeExternalLinks, rehypeExternalLinksOptions)
     .process(input);
   return output;
 }
