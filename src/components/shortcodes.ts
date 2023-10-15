81cb47bd5e6eb0994e944603e2d6249baf538b5c
cieloazul310 <cieloazul310@gmail.com>
2023-10-15 09:56:30 +0900

    chore: update

--- src/components/shortcodes.ts
+++ src/components/shortcodes.ts
@@ -1,10 +1,17 @@
 // @ts-nocheck
-import { Badge, Button, Link, PanelLink } from "@cieloazul310/astro-sarkara";
+import {
+  Alert,
+  Badge,
+  Button,
+  Link,
+  PanelLink,
+} from "@cieloazul310/astro-sarkara";
 import YouTube from "./YouTube.astro";
 import Ad from "./ads/ad.astro";
 
 export default {
   a: Link,
+  Alert,
   Badge,
   Button,
   Link,
